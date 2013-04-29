3d3438cb2360d8a03a24cfc14bd5e1674ea8a7a6
unknownhybrid <unknownhybrid@gmail.com>
2013-04-29 13:51:16 -0500

    Forgot how for-eaching worked in js.
    (Hint: it doesn't in older browsers, which is what I'm trying to support
    with this.)

--- labelfor.js
+++ labelfor.js
@@ -8,7 +8,7 @@
 */
 
 var labels = document.getElementsByTagName('label');
-foreach(var label in labels){
+for(int x = 0; x < labels.length; x++){
 	label.onclick = function () {
 		toggle(this.getAttribute('for'));
 	};

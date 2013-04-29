425584a7a80443a1da11ac31e6e741bd46176d12
unknownhybrid <unknownhybrid@gmail.com>
2013-04-29 15:20:30 -0500

    Made the script properly run on page-load.

--- labelfor.js
+++ labelfor.js
@@ -6,15 +6,15 @@
 	In IE8, clicking a label with a for attribute that targets a particular input will never toggle that input.
 	This is a bother because it means cool things like CSS3-only accordions can't exist on IE8.
 */
-
-var labels = document.getElementsByTagName('label');
-for(var x = 0; x < labels.length; x++){
-	console.log(x);
-	labels[x].onclick = function () {
-		toggle(this.getAttribute('for'));
-	};
+window.onload = function () {
+	var labels = document.getElementsByTagName('label');
+	for(var x = 0; x < labels.length; x++){
+		console.log(x);
+		labels[x].onclick = function () {
+			toggle(this.getAttribute('for'));
+		};
+	}
 }
-
 function toggle (IDfor) {
 	var elem = document.getElementById(IDfor);
 	console.log(elem.getAttribute("value"));
